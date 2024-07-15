aef69ba99f0941e477acae85c953395d98d5ae19
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-07-15 07:59:05 +0200

    Update gh-cli-autoinstall.sh
    fixed lil bugs

--- gh-cli-autoinstall.sh
+++ gh-cli-autoinstall.sh
@@ -6,12 +6,10 @@ is_alpine=$(uname -v|grep -o -w Alpine)
 is_debian=$(uname -v|grep -o -w Debian)
 # FOR ALPINE:
 if [[ "$is_alpine" == "Alpine" ]]; then _doso="doas"; _add="$_add_alpine"
-$_doso $_add git make; cd /tmp
-echo "ALPINE DETECTED, ATTEMPTING TO BUILD GH-CLI FROM SOURCE.."
-git clone https://github.com/cli/cli.git gh-cli
+echo ""; echo "ALPINE DETECTED, INSTALLING DEPENDENCIES TO BUILD FROM SOURCE.."; echo ""
+$_doso $_add go git make; cd /tmp; git clone https://github.com/cli/cli.git gh-cli
 cd gh-cli && make && $_doso make install
-echo ""; gh version; echo ""; echo "DONE, EXITING.."; echo ""; exit
-fi
+echo ""; gh version; echo ""; echo "DONE, EXITING.."; echo ""; exit; fi
 # FOR DEBIAN:
 if [[ "$is_debian" == "Debian" ]]; then (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
 && sudo mkdir -p -m 755 /etc/apt/keyrings \
@@ -20,5 +18,4 @@ if [[ "$is_debian" == "Debian" ]]; then (type -p wget >/dev/null || (sudo apt up
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
-echo ""; gh version; echo ""; echo "DONE, EXITING.."; echo ""; exit
-fi 
+echo ""; gh version; echo ""; echo "DONE, EXITING.."; echo ""; exit; fi 
